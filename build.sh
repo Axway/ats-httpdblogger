02eb57ae301b7eea2bf5d5ac8b9bd39fbe2ec70c
dslaveykov <dslaveykov@axway.com>
2017-11-02 18:54:27 +0200

    Cleanup gh-pages branch

--- build.sh
+++
@@ -1,5 +0,0 @@
-echo "Make sure you have added in PATH location to bin directories of JDK, Maven and Git client"
-# Build and install artifacts locally
-mvn -V -s settings.xml clean install $*
-# For other goals you may comment above line and uncomment next one
-#mvn -V -s settings.xml $*
